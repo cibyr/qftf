cc02fcca8faed42bbb53e076b91b1011de4b1ed7
Ian Cullinan <cibyr@cibyr.com>
2023-04-10 19:27:05 -0700

    Show QR code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
+use fltk::frame::Frame;
+use fltk::image::SvgImage;
 use fltk::{app, prelude::*, window::Window};
+use std::env;
+use qrcode::QrCode;
+use qrcode::render::svg;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let code_string = &args[1];
+
+    let code = QrCode::new(code_string).unwrap();
+    let svg = code.render::<svg::Color>()
+        .min_dimensions(400, 400)
+        .build();
+
     let app = app::App::default();
-    let mut wind = Window::new(100, 100, 400, 300, "Hello from rust");
+    let mut wind = Window::new(100, 100, 400, 400, "QFT");
+
+    let mut frame = Frame::default().with_size(400, 400).center_of(&wind);
+    let image = SvgImage::from_data(&svg).unwrap();
+    frame.set_image(Some(image));
+
     wind.end();
     wind.show();
     app.run().unwrap();
