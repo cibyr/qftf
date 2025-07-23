b8b28886d71f6de5d36493eefdd484c241c0ffe3
Ian Cullinan <cibyr@cibyr.com>
2025-07-22 22:48:59 -0700

    Cleanup and refactoring

--- src/TODO.md
+++ src/TODO.md
@@ -2,6 +2,8 @@ TODO
 
 [x] Rename to QFTF
 [x] Sending/receiving in title bar
-[ ] Progress meters
+[x] Progress meters
 [.] refactor to DRY
 [x] flag and/or env variable to configure url prefix
+[ ] write README
+[ ] screenshots
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use anyhow::Result;
+use fltk::app::App;
+use fltk::app::Receiver;
 use fltk::enums::Color;
 use fltk::frame::Frame;
 use fltk::image::SvgImage;
@@ -8,8 +10,8 @@ use human_repr::{HumanCount, HumanDuration, HumanThroughput};
 use iroh::Endpoint;
 use iroh::NodeAddr;
 use iroh::Watcher;
-use qrcode::render::svg;
 use qrcode::QrCode;
+use qrcode::render::svg;
 use rand::RngCore;
 use serde::{Deserialize, Serialize};
 use std::env;
@@ -17,10 +19,12 @@ use std::future::Future;
 use std::io;
 use std::mem::MaybeUninit;
 use std::path::Path;
-use std::time::{Instant, Duration};
+use std::time::{Duration, Instant};
+use strum::Display;
 use tokio::fs::File;
 use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
-use tracing_subscriber::{fmt, prelude::*, EnvFilter};
+use tracing::{info, warn};
+use tracing_subscriber::{EnvFilter, fmt, prelude::*};
 
 use qftf::*;
 
@@ -45,6 +49,12 @@ struct Progress {
     bytes_sent: u64,
     total_size: u64,
     finished: bool,
+}
+
+#[derive(Display)]
+enum Direction {
+    Sending,
+    Receving,
 }
 
 /// Copy data from a reader to a writer with progress callbacks
@@ -72,7 +82,7 @@ where
 {
     let buf_size = 8 * 1024; // 8KB buffer
     let mut buffer = vec![MaybeUninit::uninit(); buf_size];
-    let mut total_bytes = 0u64;
+    let mut total_bytes = 0;
 
     loop {
         let mut read_buf = tokio::io::ReadBuf::uninit(&mut buffer);
@@ -94,31 +104,98 @@ where
     Ok(total_bytes)
 }
 
-// Draw the UI
-fn show_window(url: &str, title: &str) -> (Window, Frame, ProgressBar) {
-    let code = QrCode::new(url).unwrap();
-    let svg = code
-        .render::<svg::Color>()
-        .quiet_zone(true)
-        .min_dimensions(400, 400)
-        .build();
-    let image = SvgImage::from_data(&svg).unwrap();
-    let width = image.width();
-    let height = image.height();
+struct UI {
+    app: App,
+    window: Window,
+    frame: Frame,
+    progress_bar: ProgressBar,
+}
 
-    let mut wind = Window::default().with_size(width, height).with_label(title);
+impl UI {
+    /// Create the UI (initially showing the URL code for the given URL)
+    fn create(title: &str, url: &str) -> Self {
+        let app = app::App::default();
 
-    let mut frame = Frame::default().with_size(width, height).center_of(&wind);
-    frame.set_image(Some(image));
+        let code = QrCode::new(url).unwrap();
+        let svg = code
+            .render::<svg::Color>()
+            .quiet_zone(true)
+            .min_dimensions(400, 400)
+            .build();
+        let image = SvgImage::from_data(&svg).unwrap();
+        let width = image.width();
+        let height = image.height();
 
-    let mut pb = ProgressBar::default().with_size(width, 20);
-    pb.set_selection_color(Color::Blue);
-    pb.hide();
+        let mut window = Window::default().with_size(width, height).with_label(title);
 
-    wind.end();
-    wind.show();
+        let mut frame = Frame::default().with_size(width, height).center_of(&window);
+        frame.set_image(Some(image));
 
-    (wind, frame, pb)
+        let mut progress_bar = ProgressBar::default().with_size(width, 20);
+        progress_bar.set_selection_color(Color::Blue);
+        progress_bar.hide();
+
+        window.end();
+        window.show();
+
+        Self {
+            app,
+            window,
+            frame,
+            progress_bar,
+        }
+    }
+
+    /// UI during transfer
+    fn display_progress(&mut self, direction: Direction, progress_receiver: Receiver<Progress>) {
+        let mut started = false;
+        let mut start = Instant::now();
+        let mut last_update = start - UPDATE_PERIOD;
+        while self.app.wait() {
+            if let Some(progress) = progress_receiver.recv() {
+                if progress.finished {
+                    app::quit();
+                    break;
+                }
+                let now = Instant::now();
+                if !started {
+                    if matches!(direction, Direction::Receving) {
+                        self.window
+                            .set_label(&format!("QFTF - Receiving {}", progress.name));
+                    }
+                    self.progress_bar.set_minimum(0.0);
+                    self.progress_bar.set_maximum(progress.total_size as f64);
+                    self.progress_bar.show();
+                    start = now;
+                    started = true;
+                    continue;
+                }
+                self.progress_bar.set_value(progress.bytes_sent as f64);
+                if now - last_update < UPDATE_PERIOD {
+                    continue;
+                }
+
+                let seconds_so_far = (now - start).as_secs_f64();
+                let rate = progress.bytes_sent as f64 / seconds_so_far;
+                let remaining_bytes = progress.total_size - progress.bytes_sent;
+                let remaining_time = Duration::try_from_secs_f64(remaining_bytes as f64 / rate)
+                    .map(|d| d.human_duration().to_string());
+                self.frame.set_image::<SvgImage>(None);
+                self.frame.set_label(&format!(
+                    "{} {}\n
+                    {} / {}\n
+                    {} remaining ({})",
+                    direction,
+                    progress.name,
+                    progress.bytes_sent.human_count_bytes(),
+                    progress.total_size.human_count_bytes(),
+                    remaining_time.as_deref().unwrap_or("forever"),
+                    rate.human_throughput_bytes()
+                ));
+                last_update = now;
+            }
+        }
+    }
 }
 
 // The plan:
@@ -128,7 +205,7 @@ fn show_window(url: &str, title: &str) -> (Window, Frame, ProgressBar) {
 //  * display QR code ("qftf-tx:<encoded struct>")
 //  * listen on the endpoint, waiting for receiver to supply token
 //  * stream the file over the connection
-//    * (maybe) display progress
+//    * display progress
 //  * (maybe) append some kind of hash or CRC?
 // Receiver:
 //  * create endpoint
@@ -140,7 +217,7 @@ fn show_window(url: &str, title: &str) -> (Window, Frame, ProgressBar) {
 //  * (maybe) check hash/CRC?
 // Web App:
 //  * Scan both QR codes
-//  * (maybe) display metadata, have go button?
+//  * (maybe) display metadata
 //  * Connect to receiver, send FT struct
 
 async fn send_file(path: &str) -> Result<()> {
@@ -172,16 +249,15 @@ async fn send_file(path: &str) -> Result<()> {
         token,
     };
 
-    println!("Sending {:?}", &transfer);
+    info!("Sending {:?}", &transfer);
     let transfer_json = serde_json::to_string(&transfer)?;
     let env_url = env::var(URL_PREFIX_ENV);
     let url_prefix = env_url.as_deref().unwrap_or(DEFAULT_URL_PREFIX);
     let url = format!("{url_prefix}#{TX_PREFIX}{transfer_json}");
     println!("URL: {url}");
 
-    let app = app::App::default();
     let title = format!("QFTF - Sending {}", transfer.name);
-    let (_window, mut frame, mut pb) = show_window(&url, &title);
+    let mut ui = UI::create(&title, &url);
 
     let (progress_sender, progress_receiver) = app::channel::<Progress>();
 
@@ -193,22 +269,22 @@ async fn send_file(path: &str) -> Result<()> {
             let connection = match connecting.await {
                 Ok(connection) => connection,
                 Err(cause) => {
-                    tracing::warn!("error accepting connection: {}", cause);
+                    warn!("error accepting connection: {}", cause);
                     // if accept fails, we want to continue accepting connections
                     continue;
                 }
             };
             let remote_node_id = &connection.remote_node_id()?;
-            tracing::info!("got connection from {}", remote_node_id);
+            info!("got connection from {}", remote_node_id);
             let (mut s, mut r) = match connection.accept_bi().await {
                 Ok(x) => x,
                 Err(cause) => {
-                    tracing::warn!("error accepting stream: {}", cause);
+                    warn!("error accepting stream: {}", cause);
                     // if accept_bi fails, we want to continue accepting connections
                     continue;
                 }
             };
-            tracing::info!("accepted stream from {}", remote_node_id);
+            info!("accepted stream from {}", remote_node_id);
             // read the token and verify it
             let mut buf = [0u8; 8];
             r.read_exact(&mut buf).await?;
@@ -237,7 +313,7 @@ async fn send_file(path: &str) -> Result<()> {
             .await?;
             s.finish()?;
             s.stopped().await?;
-            tracing::info!("Transfer complete!");
+            info!("Transfer complete!");
             progress_sender.send(Progress {
                 name: transfer.name,
                 bytes_sent: transfer.size,
@@ -251,46 +327,7 @@ async fn send_file(path: &str) -> Result<()> {
         Ok(())
     });
 
-    let mut started = false;
-    let mut start = Instant::now();
-    let mut last_update = start - UPDATE_PERIOD;
-    while app.wait() {
-        if let Some(progress) = progress_receiver.recv() {
-            if progress.finished {
-                app::quit();
-                break;
-            }
-            let now = Instant::now();
-            if !started {
-                pb.set_minimum(0.0);
-                pb.set_maximum(progress.total_size as f64);
-                pb.show();
-                start = now;
-                started = true;
-                continue;
-            }
-            pb.set_value(progress.bytes_sent as f64);
-            if now - last_update < UPDATE_PERIOD {
-                continue;
-            }
-
-            let seconds_so_far = (now - start).as_secs_f64();
-            let rate = progress.bytes_sent as f64 / seconds_so_far;
-            let remaining_bytes = progress.total_size - progress.bytes_sent;
-            let remaining_time = Duration::try_from_secs_f64(remaining_bytes as f64 / rate).map(|d| d.human_duration().to_string());
-            frame.set_image::<SvgImage>(None);
-            frame.set_label(&format!(
-                "Sending {}\n
-                {} / {}\n
-                {} remaining ({})",
-                progress.name, progress.bytes_sent.human_count_bytes(),
-                progress.total_size.human_count_bytes(),
-                remaining_time.as_deref().unwrap_or("forever"),
-                rate.human_throughput_bytes()
-            ));
-            last_update = now;
-        }
-    }
+    ui.display_progress(Direction::Sending, progress_receiver);
 
     println!("Done!");
     Ok(())
@@ -319,9 +356,8 @@ async fn receive_file() -> Result<()> {
 
     println!("URL: {url}");
 
-    let app = app::App::default();
     let title = "QFTF - Waiting to receive...".to_string();
-    let (mut window, mut frame, _pb) = show_window(&url, &title);
+    let mut ui = UI::create(&title, &url);
 
     let (progress_sender, progress_receiver) = app::channel::<Progress>();
 
@@ -334,22 +370,22 @@ async fn receive_file() -> Result<()> {
             let connection = match connecting.await {
                 Ok(connection) => connection,
                 Err(cause) => {
-                    tracing::warn!("error accepting connection: {}", cause);
+                    warn!("error accepting connection: {}", cause);
                     // if accept fails, we want to continue accepting connections
                     continue;
                 }
             };
             let remote_node_id = &connection.remote_node_id()?;
-            tracing::info!("got connection from {}", remote_node_id);
+            info!("got connection from {}", remote_node_id);
             let mut rs = match connection.accept_uni().await {
                 Ok(x) => x,
                 Err(cause) => {
-                    tracing::warn!("error accepting stream: {}", cause);
+                    warn!("error accepting stream: {}", cause);
                     // if accept_uni fails, we want to continue accepting connections
                     continue;
                 }
             };
-            tracing::info!("accepted stream from {}", remote_node_id);
+            info!("accepted stream from {}", remote_node_id);
             // read tx json
             let tx_json = rs.read_to_end(MAX_QR_BYTES).await?;
             let tx_json = String::from_utf8(tx_json)?;
@@ -357,7 +393,7 @@ async fn receive_file() -> Result<()> {
 
             //  * connect to sender's endpoint, send token
             let connection = endpoint.connect(transfer.node, ALPN).await?;
-            tracing::info!("Connected!");
+            info!("Connected!");
             let (mut s, r) = connection.open_bi().await?;
             s.write_all(&transfer.token).await?;
 
@@ -376,7 +412,7 @@ async fn receive_file() -> Result<()> {
                 }
             })
             .await?;
-            tracing::info!("Transfer complete!");
+            info!("Transfer complete!");
             progress_sender.send(Progress {
                 name: transfer.name,
                 bytes_sent: transfer.size,
@@ -389,26 +425,7 @@ async fn receive_file() -> Result<()> {
         Ok::<(), anyhow::Error>(())
     });
 
-    let mut got_name = false;
-    while app.wait() {
-        if let Some(progress) = progress_receiver.recv() {
-            if progress.finished {
-                app::quit();
-                break;
-            }
-            if !got_name {
-                window.set_label(&format!("QFTF - Receiving {}", progress.name));
-                got_name = true;
-            }
-
-            frame.set_image::<SvgImage>(None);
-            frame.set_label(&format!(
-                "Receiving {}\n
-                {} / {} bytes",
-                progress.name, progress.bytes_sent, progress.total_size
-            ));
-        }
-    }
+    ui.display_progress(Direction::Receving, progress_receiver);
 
     println!("Done!");
     Ok(())
